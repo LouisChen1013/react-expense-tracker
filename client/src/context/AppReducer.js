9ce145d2b723455fbb5e3a20ba8d4e5efaccea23
louis <chenhonglin1013@gmail.com>
2021-05-24 10:25:51 +0800

    updated

--- client/src/context/AppReducer.js
+++ client/src/context/AppReducer.js
@@ -1,34 +1,36 @@
 const AppReducer = (state, action) => {
-    // console.log(state)
-    // console.log(action.type)
-    switch(action.type) {
-        case 'GET_TRANSACTIONS':
-            return {
-                ...state,
-                loading: false,
-                transactions: action.payload
-            }
-        case 'DELETE_TRANSACTION':
-            // You should also avoid doing direct state-mutations(never mutate state), in redux as this will lead to side-effects. 
-            // Always try to create a deep-clone or copy of the state(spread operator) if you plan on doing some sort of mutation (deletion, update)
-            return {
-                ...state,
-                transactions: state.transactions.filter(transaction => transaction._id !== action.payload)
-            }
-        case 'ADD_TRANSACTION':
-            return {
-                ...state,
-                // transactions: [ction.payload]
-                transactions: [...state.transactions, action.payload]
-            }
-        case 'TRANSACTION_ERROR':
-            return {
-                ...state,
-                error: action.payload
-            }
-        default:
-            return state;
-    }
-}
+  // console.log(state)
+  // console.log(action.type)
+  switch (action.type) {
+    case "GET_TRANSACTIONS":
+      return {
+        ...state,
+        loading: false,
+        transactions: action.payload,
+      };
+    case "DELETE_TRANSACTION":
+      // You should also avoid doing direct state-mutations(never mutate state), in redux as this will lead to side-effects.
+      // Always try to create a deep-clone or copy of the state(spread operator) if you plan on doing some sort of mutation (deletion, update)
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction._id !== action.payload
+        ),
+      };
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        // transactions: [action.payload]
+        transactions: [...state.transactions, action.payload],
+      };
+    case "TRANSACTION_ERROR":
+      return {
+        ...state,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
-export default AppReducer
+export default AppReducer;
--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,7 +1,6 @@
-import React, { createContext, useReducer }from 'react'
-import AppReducer from './AppReducer';
-import axios from 'axios'
-
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+import axios from "axios";
 
 // Initial State for testing
 // const initialState = {
@@ -14,84 +13,93 @@ import axios from 'axios'
 // }
 
 const initialState = {
-    transactions: [],
-    error: null,
-    loading: true
-}
+  transactions: [],
+  error: null,
+  loading: true,
+};
 
 // Create context (global)
 export const GlobalContext = createContext(initialState);
 
 // Create provider component, this allows other components to access our initial state
 export const GlobalProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(AppReducer, initialState);
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    // Actions to call the reducer
+  // Actions to call the reducer
 
-    async function getTransactions() {
-        try {
-            // http://localhost:5000/api/v1/transaction since we added the proxy to our package.json, we only needed the /api/v1/transaction part
-            const res = await axios.get('/api/v1/transaction') 
-            // res.data will get our whole object, including success, count, data
-            
-            dispatch({
-                type: 'GET_TRANSACTIONS',
-                payload: res.data.data
-            })
+  async function getTransactions() {
+    try {
+      // http://localhost:5000/api/v1/transaction since we added the proxy to our package.json, we only needed the /api/v1/transaction part
+      const res = await axios.get("/api/v1/transaction");
+      // res.data will get our whole object, including success, count, data
 
-        } catch (error) {
-            dispatch({
-                type: 'TRANSACTION_ERROR',
-                payload: error.response.data.error
-            })
-        }
+      dispatch({
+        type: "GET_TRANSACTIONS",
+        payload: res.data.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: error.response.data.error,
+      });
     }
+  }
 
-    async function deleteTransaction(id) {
-        try {
-            await axios.delete(`/api/v1/transaction/${id}`)
-            
-            dispatch({
-                type: 'DELETE_TRANSACTION',
-                payload: id
-            }); 
-        } catch (error) {
-            dispatch({
-                type: 'TRANSACTION_ERROR',
-                payload: error.response.data.error
-            });
-        }
-   
+  async function deleteTransaction(id) {
+    try {
+      await axios.delete(`/api/v1/transaction/${id}`);
+
+      dispatch({
+        type: "DELETE_TRANSACTION",
+        payload: id,
+      });
+    } catch (error) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: error.response.data.error,
+      });
     }
+  }
 
-    async function addTransaction (newTransaction){
-        const config = {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
-        try {
-            const res = await axios.post('/api/v1/transaction', newTransaction, config)
+  async function addTransaction(newTransaction) {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const res = await axios.post(
+        "/api/v1/transaction",
+        newTransaction,
+        config
+      );
 
-            dispatch({
-                type: 'ADD_TRANSACTION',
-                // payload: newTransaction 
-                // we send our data (newTransaction) from res object
-                payload: res.data.data
-            })
-        } catch (error) {
-            dispatch({
-                type: 'TRANSACTION_ERROR',
-                payload: error.response.data.error
-            });
-        }
-
-
+      dispatch({
+        type: "ADD_TRANSACTION",
+        // payload: newTransaction
+        // we send our data (newTransaction) from res object
+        payload: res.data.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: error.response.data.error,
+      });
     }
-    
-    return (
-    <GlobalContext.Provider value={{ transactions: state.transactions, error: state.error, loading: state.loading, getTransactions, deleteTransaction, addTransaction, getTransactions }}>
-        {children}
-    </GlobalContext.Provider>);
-}
+  }
 
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        error: state.error,
+        loading: state.loading,
+        getTransactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
